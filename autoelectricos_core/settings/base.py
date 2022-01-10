6c88e57d1934622d0748b410f875b557a4af305f
JDavid777 <jdavidgarcia28@gmail.com>
2022-01-10 13:17:19 -0500

    deploy config

--- autoelectricos_core/settings/base.py
+++ autoelectricos_core/settings/base.py
@@ -115,9 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT='static/'
+STATIC_ROOT='autoelectricos-store.herokuapp.com/'
 MEDIA_URL= '/media/'
-
+MEDIA_ROOT= '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

236a49af5b618f5ad8760622e02c091273edcb2c
JDavid777 <jdavidgarcia28@gmail.com>
2022-01-10 11:18:03 -0500

    deploy config

--- autoelectricos_core/settings/base.py
+++ autoelectricos_core/settings/base.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,3 +121,6 @@ GRAPHENE = {
     "SCHEMA": "api_graphql.schema.ROOT_SCHEMA"
 }
 AUTH_USER_MODEL='profiles.UserProfile'
+
+
+django_heroku.settings(locals())

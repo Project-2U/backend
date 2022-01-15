4ce0cd1d288b01f65907d34cea5f168364a6522a
JDavid777 <jdavidgarcia28@gmail.com>
2022-01-15 12:22:51 -0500

    se ajusta la configuracion de desarrollo y produccion

--- autoelectricos_core/asgi.py
+++ autoelectricos_core/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoelectricos_core.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoelectricos_core.settings.production')
 
 application = get_asgi_application()
--- autoelectricos_core/settings/production.py
+++ autoelectricos_core/settings/production.py
@@ -1,12 +1,12 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-5#yfmvus%tbifzd)eub-&%r9_-pld7-sfh%)8j$+%#$l7o=b5@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [
-    'https://autoelectricos-store.herokuapp.com/'
+    'https://autoelectricos-store.herokuapp.com/',
 ]
 
 # Database

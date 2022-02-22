54f279cf8f9f0762e6620673a9dcc71b347b9499
AldairZemanate <zyiber@unicauca.edu.co>
2022-02-21 21:31:31 -0500

    cloudinary

--- electricars_core/settings.py
+++ electricars_core/settings.py
@@ -184,3 +184,11 @@ AUTH_USER_MODEL = 'profiles.UserProfile'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
